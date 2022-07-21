6b9445cadb23337492638fdd88a54e611b22b853
Willem <willemgerritzen@gmail.com>
2022-07-21 13:06:14 +0200

    Fix start_job.sh

--- start_job.sh
+++ start_job.sh
@@ -11,7 +11,7 @@ for vertices in 20 100 300 500 700 1000; do
       for image in "mondriaan" "starry_night" "mona_lisa" "the_kiss" "bach" "the_persistence_of_memory" "convergence"; do
         echo "Starting run ${run} for algorithm ${algo} on ${image} with ${vertices} vertices"
         name="${run}"_"${algo}"_"${image}"_"${vertices}"
-        sbatch -J $name job "$algo" "$run" "$image" "$vertices"
+        sbatch -J $name job.sh "$algo" "$run" "$image" "$vertices"
       done
     done
   done
